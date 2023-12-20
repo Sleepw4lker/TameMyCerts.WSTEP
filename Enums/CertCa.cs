43a4f402cf419a19e4b5931f9a11f61d78d2eeb7
Uwe Gradenegger <uwe@gradenegger.eu>
2023-12-20 14:29:33 +0100

    Move to shared project for common classes

--- Enums/CertCa.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TameMyCerts.WSTEP.Enums
-{
-    // Constants from CertCa.h
-    internal static class CertCa
-    {
-        public const int CT_FLAG_ENROLLEE_SUPPLIES_SUBJECT = 1;
-    }
-}
